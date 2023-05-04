2beca47311aaa60a6c8c5231bd50cdb5658e4a28
George <71167099+letow@users.noreply.github.com>
2023-05-05 03:46:36 +0500

    added logging form info to console + refactored

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,7 +1,6 @@
 import {
     Button,
     Select,
-    View,
     Panel,
     PanelHeader,
     Group,
@@ -13,22 +12,52 @@ import {
     LocaleProvider,
 } from "@vkontakte/vkui";
 import "./Form.css";
+import dayjs from "dayjs";
 import { useEffect, useState } from "react";
 import { TimePicker } from "@mui/x-date-pickers/TimePicker/TimePicker";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider/LocalizationProvider";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
-import dayjs from "dayjs";
+import { OptionType } from "../../types/OptionType";
+import { useMediaQuery } from "@mui/material";
 
 export const Form = () => {
+    const isMobile = useMediaQuery("(max-width: 768px)");
+
+    const towerOptions: OptionType[] = [
+        {
+            value: "A",
+            label: "Башня А",
+        },
+        {
+            value: "B",
+            label: "Башня Б",
+        },
+    ];
+
+    const roomOptions: OptionType[] = new Array(10).fill(0).map((_, i) => {
+        return {
+            value: (i + 1).toString(),
+            label: (i + 1).toString(),
+        };
+    });
+
+    const floorOptions: OptionType[] = new Array(25).fill(0).map((_, i) => {
+        return {
+            value: (i + 3).toString(),
+            label: (i + 3).toString(),
+        };
+    });
+
     const [tower, setTower] = useState<string>("");
     const [floor, setFloor] = useState<string>("");
     const [room, setRoom] = useState<string>("");
     const [date, setDate] = useState<Date | undefined>(() => new Date());
-    const [isError, setIsError] = useState(true);
     const [text, setText] = useState<string>("");
+    const [isError, setIsError] = useState<boolean>(true);
 
-    const minTime = dayjs().set("hour", 8).startOf("hour");
-    const maxTime = dayjs().set("hour", 22).startOf("hour");
+    // assume that conference rooms available from 9 to 21 (while VK office is open)
+    const minTime = dayjs().set("hour", 9).startOf("hour");
+    const maxTime = dayjs().set("hour", 21).startOf("hour");
 
     const [startTime, setStartTime] = useState<dayjs.Dayjs | null>(null);
     const [endTime, setEndTime] = useState<dayjs.Dayjs | null>(null);
@@ -43,13 +72,29 @@ export const Form = () => {
         setText("");
     };
 
+    const sendInfo = () => {
+        console.log(
+            JSON.stringify({
+                tower,
+                floor,
+                room,
+                date: new Intl.DateTimeFormat("ru").format(date),
+                startTime: startTime?.format("HH:mm"),
+                endTime: endTime?.format("HH:mm"),
+                text,
+            })
+        );
+    };
+
     const onChangeTextarea = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
         setText(e.target.value);
     };
 
     useEffect(() => {
-        if (!startTime || !endTime) return;
+        if (!startTime || !endTime) return; // if times are empty
 
+        // check if all fields are not empty
+        // else enable the send button
         startTime.isAfter(endTime, "minutes") ||
         startTime.isSame(endTime, "minutes") ||
         !tower ||
@@ -61,147 +106,128 @@ export const Form = () => {
     }, [startTime, endTime, tower, floor, room, date]);
 
     return (
-        <View activePanel="new-book">
-            <Panel id="new-book">
-                <PanelHeader>Бронирование переговорки</PanelHeader>
-                <Group>
-                    <FormLayout id="bookForm">
-                        <FormLayoutGroup mode="horizontal">
-                            <FormItem
-                                top="Башня"
-                                status={tower ? "valid" : "error"}
-                            >
-                                <Select
-                                    placeholder="Выберите башню"
-                                    value={tower}
-                                    allowClearButton
-                                    options={[
-                                        {
-                                            value: "A",
-                                            label: "Башня А",
-                                        },
-                                        {
-                                            value: "B",
-                                            label: "Башня Б",
-                                        },
-                                    ]}
-                                    onChange={(e) => setTower(e.currentTarget.value)}
-                                />
-                            </FormItem>
-
-                            <FormItem
-                                top="Этаж"
-                                status={floor ? "valid" : "error"}
-                            >
-                                <Select
-                                    placeholder="Выберите этаж"
-                                    value={floor}
-                                    allowClearButton
-                                    options={new Array(25).fill(0).map((_, i) => {
-                                        return {
-                                            value: (i + 3).toString(),
-                                            label: (i + 3).toString(),
-                                        };
-                                    })}
-                                    onChange={(e) => setFloor(e.currentTarget.value)}
-                                />
-                            </FormItem>
-
-                            <FormItem
-                                top="Переговорка"
-                                status={room ? "valid" : "error"}
-                            >
-                                <Select
-                                    placeholder="Выберите переговорку"
-                                    value={room}
-                                    allowClearButton
-                                    options={new Array(10).fill(0).map((_, i) => {
-                                        return {
-                                            value: (i + 1).toString(),
-                                            label: (i + 1).toString(),
-                                        };
-                                    })}
-                                    onChange={(e) => setRoom(e.currentTarget.value)}
-                                />
-                            </FormItem>
-                        </FormLayoutGroup>
-
-                        <FormLayoutGroup mode="horizontal">
-                            <div className="timePickerBlock">
-                                <FormItem top={"Дата бронирования"}>
-                                    <LocaleProvider value={"ru"}>
-                                        <DateInput
-                                            value={date}
-                                            disablePast
-                                            disablePickers
-                                            showNeighboringMonth
-                                            onChange={setDate}
-                                        />
-                                    </LocaleProvider>
-                                </FormItem>
-                            </div>
-
-                            <FormItem
-                                top={"Время бронирования"}
-                                className="timePickerFormItem"
-                            >
-                                <LocalizationProvider dateAdapter={AdapterDayjs}>
-                                    <div className="timePickerBlock">
-                                        <TimePicker
-                                            className="timePicker"
-                                            value={startTime}
-                                            minTime={minTime}
-                                            maxTime={maxTime}
-                                            format={"HH:mm"}
-                                            ampm={false}
-                                            skipDisabled
-                                            onChange={(e) => setStartTime(e)}
-                                        />
-                                        <span>—</span>
-                                        <TimePicker
-                                            className="timePicker"
-                                            value={endTime}
-                                            minTime={minTime}
-                                            maxTime={maxTime}
-                                            format={"HH:mm"}
-                                            ampm={false}
-                                            skipDisabled
-                                            onChange={(e) => setEndTime(e)}
-                                        />
-                                    </div>
-                                </LocalizationProvider>
-                            </FormItem>
-                        </FormLayoutGroup>
-
-                        <FormItem top="Комментарий">
-                            <Textarea
-                                value={text}
-                                onChange={onChangeTextarea}
+        <Panel id="new-book">
+            <PanelHeader>Бронирование переговорной</PanelHeader>
+            <Group className="group">
+                <FormLayout id="bookForm">
+                    <FormLayoutGroup mode={isMobile ? "vertical" : "horizontal"}>
+                        <FormItem
+                            top="Башня"
+                            status={tower ? "valid" : "error"}
+                        >
+                            <Select
+                                placeholder="Выберите башню"
+                                value={tower}
+                                allowClearButton
+                                options={towerOptions}
+                                onChange={(e) => setTower(e.currentTarget.value)}
                             />
                         </FormItem>
 
-                        <FormItem>
-                            <Button
-                                size="l"
-                                stretched
-                                disabled={isError}
-                            >
-                                Отправить
-                            </Button>
+                        <FormItem
+                            top="Этаж"
+                            status={floor ? "valid" : "error"}
+                        >
+                            <Select
+                                placeholder="Выберите этаж"
+                                value={floor}
+                                allowClearButton
+                                options={floorOptions}
+                                onChange={(e) => setFloor(e.currentTarget.value)}
+                            />
                         </FormItem>
-                        <FormItem>
-                            <Button
-                                size="l"
-                                appearance="neutral"
-                                stretched
-                                onClick={clearForm}
-                            >
-                                Очистить
-                            </Button>
+
+                        <FormItem
+                            top="Переговорная"
+                            status={room ? "valid" : "error"}
+                        >
+                            <Select
+                                placeholder="Выберите переговорную"
+                                value={room}
+                                allowClearButton
+                                options={roomOptions}
+                                onChange={(e) => setRoom(e.currentTarget.value)}
+                            />
                         </FormItem>
-                    </FormLayout>
-                </Group>
-            </Panel>
-        </View>
+                    </FormLayoutGroup>
+
+                    <FormLayoutGroup mode={isMobile ? "vertical" : "horizontal"}>
+                        <div className="datePickerBlock">
+                            <FormItem top={"Дата бронирования"}>
+                                <LocaleProvider value={"ru"}>
+                                    <DateInput
+                                        value={date}
+                                        status={date ? "valid" : "error"}
+                                        disablePast
+                                        disablePickers
+                                        showNeighboringMonth
+                                        onChange={setDate}
+                                    />
+                                </LocaleProvider>
+                            </FormItem>
+                        </div>
+
+                        <FormItem
+                            top={"Время бронирования"}
+                            className="timePickerFormItem"
+                        >
+                            <LocalizationProvider dateAdapter={AdapterDayjs}>
+                                <div className="timePickerBlock">
+                                    <TimePicker
+                                        className="timePicker"
+                                        value={startTime}
+                                        minTime={minTime}
+                                        maxTime={maxTime}
+                                        format={"HH:mm"}
+                                        ampm={false}
+                                        skipDisabled
+                                        onChange={(e) => setStartTime(e)}
+                                    />
+                                    <span>—</span>
+                                    <TimePicker
+                                        className="timePicker"
+                                        value={endTime}
+                                        minTime={minTime}
+                                        maxTime={maxTime}
+                                        format={"HH:mm"}
+                                        ampm={false}
+                                        skipDisabled
+                                        onChange={(e) => setEndTime(e)}
+                                    />
+                                </div>
+                            </LocalizationProvider>
+                        </FormItem>
+                    </FormLayoutGroup>
+
+                    <FormItem top="Комментарий">
+                        <Textarea
+                            value={text}
+                            onChange={onChangeTextarea}
+                        />
+                    </FormItem>
+
+                    <FormItem>
+                        <Button
+                            size="l"
+                            stretched
+                            disabled={isError}
+                            onClick={sendInfo}
+                        >
+                            Отправить
+                        </Button>
+                    </FormItem>
+                    <FormItem>
+                        <Button
+                            size="l"
+                            appearance="neutral"
+                            stretched
+                            onClick={clearForm}
+                        >
+                            Очистить
+                        </Button>
+                    </FormItem>
+                </FormLayout>
+            </Group>
+        </Panel>
     );
 };
